f98fc3b33032e48e35fe0dbc8f768af818c6f40e
s-nt-s <santos82h@gmail.com>
2018-10-04 11:49:35 +0200

    codigo temario

--- crear_pdf.py
+++ crear_pdf.py
@@ -9,14 +9,21 @@ from util import html_to_pdf
 
 num = re.compile(r"_(\d+).html")
 
-files = sys.argv[1:] or sorted(iglob("./**/*.html", recursive=True))
-for h in files:
-    fname = os.path.basename(h)
+def get_codigo(fname):
+    if fname == "temario.html":
+        return "GSI"
     m = num.search(fname)
     if m:
         codigo = fname[0].upper() + m.group(1)
     else:
         codigo = fname[0:2].upper()
+    return codigo
+    
+
+files = sys.argv[1:] or sorted(iglob("./**/*.html", recursive=True))
+for h in files:
+    fname = os.path.basename(h)
+    codigo = get_codigo(fname)
     print("Convirtiendo " + fname + " a PDF ...", end="\r")
     html_to_pdf(h, codigo)
     print("Convirtiendo " + fname + " a PDF 100%")
