b14051239331bbc7804ec166cd30f0a5e834f254
s-nt-s <santos82h@gmail.com>
2021-07-26 19:00:12 +0200

    dates

--- tools/dates.sh
+++ tools/dates.sh
@@ -2,12 +2,12 @@
 cd "$(dirname "$0")/.."
 rm -f config/dates.txt
 find content/ -name '*.md' -print0 | while IFS= read -r -d '' file; do
-    CR=$(git log --format=%ai "$file" | head -n 1)
     UP=$(git log -1 --format=%ai "$file")
+    CR=$(git log --format=%ai "$file" | tail -n 1)
     echo "$file" >> config/dates.txt
-    echo "$CR" >> config/dates.txt
     echo "$UP" >> config/dates.txt
-    let DIFF=($(date +%s -d "$CR")-$(date +%s -d "$UP"))/86400
+    echo "$CR" >> config/dates.txt
+    let DIFF=($(date +%s -d "$UP")-$(date +%s -d "$CR"))/86400
     echo $DIFF >> config/dates.txt
     echo "" >> config/dates.txt
 done
