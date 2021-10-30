0c5dfd99f618738edfccf8ab60cc5d2b64da9c15
s-nt-s <santos82h@gmail.com>
2021-10-30 19:25:07 +0200

    inap

--- scripts/inap.py
+++ scripts/inap.py
@@ -1,9 +1,14 @@
 import re
 from textwrap import dedent
 from time import sleep
+from functools import lru_cache
+import sys
 
 from munch import Munch
 from unidecode import unidecode
+
+from os import makedirs
+from os.path import dirname
 
 from .core.util import clean_url, read, url_key, write
 from .core.web import FF, Web
@@ -67,15 +72,19 @@ class CrawlInap:
         urls = self.w.soup.select(slc)
         r = []
         done = set()
+        s = []
         for i in urls:
             url = i.attrs["href"]
             if url in done:
                 continue
             txt = re_sp.sub(" ", i.get_text()).strip()
             if text is None or text.search(txt):
-                r.append((txt, url))
                 done.add(url)
-        return r
+                if txt == "Documentación común para resolución Supuestos 7 y 8 Carpeta":
+                    s.append((txt, url))
+                else:
+                    r.append((txt, url))
+        return r+s
 
     def get_text_nodes(self, tag, rg):
         main = self.w.soup.select_one("#region-main")
@@ -88,26 +97,42 @@ class CrawlInap:
                     txt = mtc.group(1)
                 yield txt.strip()
 
-    def get_bloques(self):
+    def get_supuestos(self):
+        pass
+
+    def get_bloques(self, descargar=False):
+        def getB(s):
+            for i, b in enumerate(["BLOQUE IV", "BLOQUE III", "BLOQUE II", "BLOQUE I"]):
+                if b in s:
+                    return 4-i
+            if s == "SIMULACRO DE EXAMEN":
+                return 5
+            return None
+
         blq = 0
         URLS = self.get_urls("div.content h3.section-title a")
         for bloque, url in URLS:
             print("")
             print(bloque)
             print(url)
+            blq = getB(bloque)
+            if blq is None:
+                continue
+            titulo = bloque.rsplit("-", 1)[-1]
+            titulo = titulo.strip()
+            titulo = titulo.capitalize()
+
             self.w.get(url)
             TEST = self.get_urls("div.content a.aalink", text=re_test)
             if TEST:
                 TEMAS = list(self.get_text_nodes("p", re_tema))
-                blq = blq + 1
-                titulo = bloque.split("-", 1)[-1]
-                titulo = titulo.strip()
-                titulo = titulo.capitalize()
                 b = Munch(
                     bloque=blq,
                     titulo=titulo,
                     url=url,
-                    temas=[]
+                    tipo="Tests",
+                    temas=[],
+                    supuestos=[],
                 )
                 print(len(TEMAS), "temas", len(TEST), "test")
                 while len(TEMAS)<len(TEST):
@@ -120,7 +145,7 @@ class CrawlInap:
                         url=href,
                         feedback={},
                         leyes={},
-                        done=0
+                        done=0,
                     )
                     b.temas.append(t)
                     self.w.get(href)
@@ -138,30 +163,70 @@ class CrawlInap:
                             if ley not in ("Real Decreto 211/2019", ):
                                 t.leyes[ley] = t.leyes.get(ley, 0) + 1
                 yield b
+            else:
+                b = Munch(
+                    bloque=blq,
+                    titulo=titulo,
+                    url=url,
+                    tipo="Supuestos",
+                    temas=[],
+                    supuestos=[],
+                )
+                SUPUESTOS = self.get_urls("div.single-section ul.section.img-text div.activityinstance a")
+                for i, (txt, href) in enumerate(SUPUESTOS):
+                    print(" ", "%2d" % (i+1), txt)
+                    t = Munch(
+                        supuesto=i+1,
+                        titulo=txt,
+                        url=href,
+                        archivos=[]
+                    )
+                    b.supuestos.append(t)
+                    self.w.get(href)
+                    for txt, href in self.get_urls("div[role='main'] a"):
+                        if href != "#":
+                            t.archivos.append(Munch(titulo=txt, url=href))
+                    if descargar:
+                        for fl in t.archivos:
+                            trg = "debug/inap/B{}/{}/{}".format(b.bloque, t.titulo, fl.titulo)
+                            makedirs(dirname(trg), exist_ok=True)
+                            r = self.w.s.get(fl.url)
+                            with open(trg, "wb") as f:
+                                f.write(r.content)
+                yield b
 
-    def save(self):
+
+    def save(self, descargar=False):
         feedback = {}
         leyes = {}
         MD = []
         def dict_add(d1, d2):
             for k, v in d2.items():
                 d1[k] = d1.get(k, 0) + v
-        for b in self.get_bloques():
-            MD.append("\n{bloque}. [{titulo}]({url})".format(**dict(b)))
+        lastTipo = None
+        for b in self.get_bloques(descargar=descargar):
+            if b.tipo is not lastTipo:
+                MD.append("\n# "+b.tipo+"\n")
+                lastTipo = b.tipo
+            MD.append("{bloque}. [{titulo}]({url})".format(**dict(b)))
             for t in b.temas:
                 MD.append("    {tema}. [{titulo}]({url}) <small>[{done}]</small>".format(**dict(t)))
                 dict_add(feedback, t.feedback)
                 dict_add(leyes, t.leyes)
+            for t in b.supuestos:
+                MD.append("    {supuesto}. [Supuesto {supuesto}]({url})".format(**dict(t)))
+                for index, a in enumerate(t.archivos):
+                    MD.append("        {i}. [{titulo}]({url})".format(i=index+1, **dict(a)))
 
         leyes = sorted(leyes.items(), key=lambda x:(-x[1], x[0]))
         if leyes:
-            MD.append("\nLeyes mencionadas en algunos de los tests:\n")
+            MD.append("\n# Leyes mencionadas en algunos de los tests:\n")
             for a, count in leyes:
                 MD.append("* {} x {}".format(count, a))
         feedback = sorted(feedback.items(), key=lambda x:(-x[1], url_key(x[0])))
         if feedback:
             MD.append(
-                "\nURLs referenciadas en las soluciones de algunos de los tests:\n")
+                "\n# URLs referenciadas en las soluciones de algunos de los tests:\n")
             for a, count in feedback:
                 if "_" in a:
                     MD.append('* {2} x <a href="{1}">{0}</a>'.format(clean_url(a), a, count))
@@ -182,4 +247,4 @@ class CrawlInap:
 
 if __name__ == "__main__":
     c = CrawlInap("content/posts/ejercicios/10-")
-    c.save()
+    c.save(descargar="dwn" in sys.argv)
